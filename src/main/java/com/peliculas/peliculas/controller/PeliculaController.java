adac069e08b6bbec6c762b2ad2bc5a60ee37b20e
Sebastián Catalán Hermosilla <scatalanhermosilla@gmail.com>
2024-04-15 15:00:50 -0400

    6 Commit services y controller pelicula

--- src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
+++ src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
@@ -1,46 +1,65 @@
 package com.peliculas.peliculas.controller;
 
+import com.peliculas.peliculas.model.Pelicula;
+import com.peliculas.peliculas.service.PeliculaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/peliculas")
+@RequestMapping("/api/peliculas")
 public class PeliculaController {
 
+    private final PeliculaService peliculaService;
+
     @Autowired
-    private PeliculaRepository peliculaRepository;
-
-    @GetMapping("/")
-    public List<Pelicula> obtenerTodasLasPeliculas() {
-        return peliculaRepository.findAll();
+    public PeliculaController(PeliculaService peliculaService) {
+        this.peliculaService = peliculaService;
     }
 
+    // Endpoint para obtener todas las películas
+    @GetMapping
+    public ResponseEntity<List<Pelicula>> getAllPeliculas() {
+        List<Pelicula> peliculas = peliculaService.getAllPeliculas();
+        return new ResponseEntity<>(peliculas, HttpStatus.OK);
+    }
+
+    // Endpoint para obtener una película por su ID
     @GetMapping("/{id}")
-    public Pelicula obtenerPeliculaPorId(@PathVariable Long id) {
-        return peliculaRepository.findById(id).orElse(null);
-    }
-
-    @PostMapping("/")
-    public Pelicula crearPelicula(@RequestBody Pelicula pelicula) {
-        return peliculaRepository.save(pelicula);
-    }
-
-    @PutMapping("/{id}")
-    public Pelicula actualizarPelicula(@PathVariable Long id, @RequestBody Pelicula peliculaActualizada) {
-        Pelicula pelicula = peliculaRepository.findById(id).orElse(null);
+    public ResponseEntity<Pelicula> getPeliculaById(@PathVariable("id") Long id) {
+        Pelicula pelicula = peliculaService.getPeliculaById(id);
         if (pelicula != null) {
-            pelicula.setTitulo(peliculaActualizada.getTitulo());
-            pelicula.setDirector(peliculaActualizada.getDirector());
-            // Actualizar otros atributos
-            return peliculaRepository.save(pelicula);
+            return new ResponseEntity<>(pelicula, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return null; // Manejar el caso cuando no se encuentra la película
     }
 
+    // Endpoint para agregar una nueva película
+    @PostMapping
+    public ResponseEntity<Pelicula> createPelicula(@RequestBody Pelicula pelicula) {
+        Pelicula nuevaPelicula = peliculaService.createPelicula(pelicula);
+        return new ResponseEntity<>(nuevaPelicula, HttpStatus.CREATED);
+    }
+
+    // Endpoint para actualizar una película existente
+    @PutMapping("/{id}")
+    public ResponseEntity<Pelicula> updatePelicula(@PathVariable("id") Long id, @RequestBody Pelicula pelicula) {
+        Pelicula peliculaActualizada = peliculaService.updatePelicula(id, pelicula);
+        if (peliculaActualizada != null) {
+            return new ResponseEntity<>(peliculaActualizada, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Endpoint para eliminar una película
     @DeleteMapping("/{id}")
-    public void eliminarPelicula(@PathVariable Long id) {
-        peliculaRepository.deleteById(id);
+    public ResponseEntity<Void> deletePelicula(@PathVariable("id") Long id) {
+        peliculaService.deletePelicula(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
